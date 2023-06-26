d4fc0addb00845ad3a11397bd493c699e3512251
Avery <75399033+overworded@users.noreply.github.com>
2023-06-26 18:56:14 -0500

    Update FallDamage.lua

--- FallDamage.lua
+++ FallDamage.lua
@@ -1,17 +1,16 @@
-local function FallDmg(Plr: Player, Char: Instance)
-	local Velocity = math.abs(Char.HumanoidRootPart.Velocity.Y) -- calculating velocity
-	local Damage = (Velocity / 70) ^ 4.5 -- how much damage will be taken, you may have to play with this
-
-	if Velocity >= 80 then -- threshold for fall damge to be taken
-		Char.Humanoid:TakeDamage(Damage) -- deal damage to player
+local function FallDmg(Char: Instance)
+	local Velocity = math.abs(Char.HumanoidRootPart.Velocity.Y) -- calculating velocity.
+	local Damage = (Velocity / 70) ^ 4.5 -- how much damage will be taken, you may have to play with these numbers as they are low right now.
+	if Velocity >= 80 then -- threshold for fall damage to be taken.
+		Char.Humanoid:TakeDamage(Damage) -- deal damage to the player.
 	end
 end
 
-game.Players.PlayerAdded:Connect(function(Plr) -- player joins the game
-	Plr.CharacterAdded:Connect(function(Char) -- players character is created
-		Char.Humanoid.StateChanged:Connect(function(oldState, newState) -- check for fall dmg
-			if newState == Enum.HumanoidStateType.Landed then -- if the state is landed
-				FallDmg(Plr, Char) -- do fall dmg
+game.Players.PlayerAdded:Connect(function(Plr) -- player joins the game.
+	Plr.CharacterAdded:Connect(function(Char) -- players character is created.
+		Char.Humanoid.StateChanged:Connect(function(oldState, newState) -- check for potential falls.
+			if newState == Enum.HumanoidStateType.Landed then -- if the state is landed (they fell).
+				FallDmg(Char) -- do fall damage.
 			end
 		end)
 end)
